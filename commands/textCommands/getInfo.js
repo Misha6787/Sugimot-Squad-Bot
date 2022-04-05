62e4edb0cc58d0980462b5ff56748504197dc4d1
Миша Филимон <misafilimon255@mail.ru>
2022-04-05 21:42:45 +0300

    fix bugs

--- commands/textCommands/getInfo.js
+++ commands/textCommands/getInfo.js
@@ -6,24 +6,9 @@ module.exports = async (bot,message,args,argsF) => {
 
     const User = await bot.User.find({guildId: message.guildId});
     const thisUser = await bot.User.findOne({id: message.author.id, guildId: message.guildId});
-    let guild = await bot.guilds.fetch('777301995803377704');
-    const days = 2;
-    const today = new Date()
-    const tomorrow = new Date(today)
 
-    User.forEach(item => {
-        if (thisUser.id === item.id) {
-            //console.log(new Date(item.permissions.mute_members.date))
-            //console.log(new Date(dateNow).getTime())
-            //console.log(new Date(dateNow + active_days).getTime())
-            //tomorrow.setDate(tomorrow.getDate() + days)
-            const member = guild.members.cache.get(User.id)
-            console.log(member)
-        }
-        // if (new Date(item.permissions.mute.members.date) >= new Date()) {
-        //
-        // }
-    })
+    //console.log(thisUser.permissions.mute_members)
+    //console.log(thisUser.permissions.move_members)
 
 
     //console.log(User)
